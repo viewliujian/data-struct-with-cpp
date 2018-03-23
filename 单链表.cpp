b626c36a793a336803dfbe37d150b34e64fad1cc
viewliujian <37686407+viewliujian@users.noreply.github.com>
2018-03-23 22:56:53 +0800

    Create 单链表.cpp

---
+++ 单链表.cpp
@@ -0,0 +1,67 @@
+<第二代描述>
+****关于单链表****
+1.单链表结点描述
+typedef struct node
+{
+  ElemType data;
+  struct node *next;  
+}LinkListNode;
+
+2.单链表的初始化
+/*=================================
+函数功能：单链表运算————初始化
+函数输入：无
+函数输出：单链表头指针
+==================================*/
+LinkListNode *initialize_Lklist(void)
+{
+  LinkListNode *head;
+  head = (LinkListNode *)malloc(sizeof(LinkListNode));
+  if(head = NULL)
+    exit(1);
+  head->next = NULL;
+  return head;
+}
+
+3.建立单链表
+  1）尾插法建立单链表：不断在单链表的尾部插入结点；
+  /*=================================
+  函数功能：单链表操作————尾插法建立单链表
+  函数输入：结点值数组，结点个数
+  函数输出：单链表的头指针
+  ==================================*/
+  LinkListNode *Create_Rear_List(ElemType a[], int n)
+  {
+    LinkList *head, *p, *q;
+    
+    head = (LinkListNode*)malloc(sizeof(ListLinkNode));
+    q = head;
+    
+    for(int i = 0; i < n; i++)
+    {
+      p = (LinkListNode)malloc(sizeof(LinkListNode));
+      p->data = a[i];
+      q->next = p;
+      q = p;
+    }
+    p->next = NULL;
+    return head;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
