5af090c4c3ef8f8ddbe334069e269a75c78f9ca9
viewliujian <37686407+viewliujian@users.noreply.github.com>
2018-03-23 23:35:21 +0800

    Update 单链表.cpp

--- 单链表.cpp
+++ 单链表.cpp
@@ -16,7 +16,7 @@ typedef struct node
 LinkListNode *initialize_Lklist(void)
 {
   LinkListNode *head;
-  head = (LinkListNode *)malloc(sizeof(LinkListNode));
+  head = (LinkListNode*)malloc(sizeof(LinkListNode));
   if(head = NULL)
     exit(1);
   head->next = NULL;
@@ -32,19 +32,44 @@ LinkListNode *initialize_Lklist(void)
   ==================================*/
   LinkListNode *Create_Rear_List(ElemType a[], int n)
   {
-    LinkList *head, *p, *q;
+    LinkList *head, *p, *q;  //p当前结点，q前驱结点
     
     head = (LinkListNode*)malloc(sizeof(ListLinkNode));
     q = head;
     
     for(int i = 0; i < n; i++)
     {
-      p = (LinkListNode)malloc(sizeof(LinkListNode));
+      p = (LinkListNode*)malloc(sizeof(LinkListNode));
       p->data = a[i];
       q->next = p;
       q = p;
     }
     p->next = NULL;
+    return head;
+  }
+  1）头插法建立单链表方法1：不断在单链表的头部插入结点；
+  /*=================================
+  函数功能：单链表操作————头插法建立单链表方法1
+  函数输入：结点值数组，结点个数
+  函数输出：单链表的头指针
+  ==================================*/
+  LinkListNode *Create_Front1_LkList(ElemType a[], int n)
+  {
+    LinkList *head, *p, *q;
+    
+    head = (LinkListNode*)malloc(sizeof(LinkListNode));
+    head->next = NULL;
+    q = head->next;
+    
+    for(i = n - 1; i >= 0; i--)
+    {
+      p = (LinkListNode*)malloc(sizeof(LinkListNode));
+      p->data = a[i];
+      p->next = q;
+      
+      head->next = p;
+      q = head->next;
+    }
     return head;
   }
 
